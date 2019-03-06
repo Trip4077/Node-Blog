b18b1913cea1f1ae3a63744107253f3cf397dbcc
Trip4077 <bj2336@email.vccs.edu>
2019-03-05 21:32:25 -0500

    POST new post complete

--- api/postRoutes.js
+++ api/postRoutes.js
@@ -2,12 +2,44 @@ const express = require('express');
 const router = express.Router();
 const db = require ('../data/helpers/postDb');
 
+//Create new post
+router.post('/', async (req, res) => {
+    try {
+        const post = req.body;
+        console.log(post)
+        if(post.text && post.user_id) {
+            const posted = await db.insert(post);
+            res.status(201).json(posted)
+        } else {
+            res.status(400).json({ errorMessage: "Please provide text for the post." })
+        }
+    } catch(err) {
+        res.status(500).json({ error: 'Post could not be added'})
+    }
+});
+
+//Get all posts
 router.get('/', async (req, res) => {
     try {
         const posts = await db.get();
         res.status(200).json(posts);
     } catch(err) {
         res.status(500).json({ error: "The posts information could not be retrieved." });
+    }
+});
+
+//Get post by id
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await db.getById(req.params.id);
+
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    } catch(err) {
+        res.status(500).json({ error: "The user information could not be retrieved." });
     }
 })
 
