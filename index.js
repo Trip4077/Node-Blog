73cdbc7da3621113ed153825e9a76dff49999d3f
Trip4077 <bj2336@email.vccs.edu>
2019-03-07 16:05:15 -0500

    Pre-Deploy Update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const server = require('./server');
+require('dotenv').config();
 
-const PORT = '5000';
+const server = require('./server');
+const PORT = process.env.PORT || '5000';
 
 server.listen(PORT, () => {
     console.log(`*** Server listening on port:${PORT}`)
